39c358b460cb67f711424643e68fed9727aa4cc7
Victor Garnier <victorgarnier44@gmail.com>
2024-10-30 11:47:37 +0000

    Timestamp to date

--- models/mart/finance/finance_campaign_day.sql
+++ models/mart/finance/finance_campaign_day.sql
@@ -1,5 +1,5 @@
 SELECT
-    date_date,
+    CAST(date_date AS DATE) AS date_date,
     ROUND((operational_margin - ads_cost),2) AS ads_margin,
     average_basket,
     operational_margin,
@@ -10,6 +10,6 @@ SELECT
     purchase_cost,
     shipping_fee,
     logcost,
-FROM {{ref("int_campaign_day")}} AS c
-JOIN {{ref("finance_days")}} AS f
-ON "c.date_date" = "f.data_date"
+FROM {{ref("int_campaign_day")}}
+JOIN {{ref("finance_days")}}
+USING (date_date)
