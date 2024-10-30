44ac144c21e7e020a988fdd1a92c62a0f5661d3d
Victor Garnier <victorgarnier44@gmail.com>
2024-10-30 13:38:21 +0000

    Adding mergin_percent macro

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,7 @@
 SELECT *
     ,ROUND((s.quantity * p.purchase_price),2) AS purchase_cost
     ,ROUND((s.revenue - (s.quantity * p.purchase_price))) AS margin
+    ,{{ margin_percent( "revenue", "quantity * purchase_price" ) }} AS margin_percent
 FROM {{ref('stg_raw__sales')}} AS s
     JOIN {{ref('stg_raw__product')}} AS p
 USING (products_id)
